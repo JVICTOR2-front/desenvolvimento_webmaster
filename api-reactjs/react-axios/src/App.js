d952658a9b8db2292eb059b3fad505128ed11850
João Victor Batista De Souza <res0099170@aluno.firjansenaisesi.com.br>
2026-02-26 21:31:36 -0300

    desenvolvimento

--- api-reactjs/react-axios/src/App.js
+++ api-reactjs/react-axios/src/App.js
@@ -3,53 +3,53 @@ import api from "./services/api";
 import './App.css';
 
 export default function App() {
-    const[username, setUsername] = useState('JVICTOR2-front');
+    const [username, setUsername] = useState('JVICTOR2-front');
     const[user, setUser] = useState(null);
-    const [repos, setRepos] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const[repos, setRepos] = useState([]);
+    const[loading, setLoading] = useState(true);
+    const[error, setError] = useState(null);
+    
     
     //carrega dados da API
     useEffect(() =>{
-      async function loadData(userToSearch= username){
+      async function loadData( userToSearch = username){
         try{
           setLoading(true);
           setError(null);
-           //Carrega usuario
-          const userResponse = await api.get(`/users/${userToSearch}`);
-          setUser(userResponse.data);
 
-            const reposResponse = await api.get(`/users/${userToSearch}/repos?per_page=6`);
-          setUser(userResponse.data); 
+          //Carrega usuário
 
-          } catch(error){
-            console.error(' Erro ', error)
-            setError(`Usuparui ${userToSearch} nao encontrado`);
+          const userResponse = await api.get(`/users/${userToSearch}`); //API Responde
+          setUser(userResponse.data); //Extrai os dados da API
 
+          const reposResponse = await api.get(`/users/${userToSearch}/repos?per_page=6&sort=updated`); 
+          setRepos(reposResponse.data); 
 
-          } finally{
-            setLoading(false);
-          }
-           loadData();
-          }
-      }, []);
-    
+        } catch(error){
+          console.error('Erro', error);
+          setError(`Usuário ${userToSearch} não encontrado`);
+
+        } finally{
+          setLoading(false);
+        }
+      }
+      loadData();
+    }, []);
+
     if(loading){
       return (
         <div className="loading-container">
           <div className="spinner"></div>
-            <p>Carregando perfil...</p>
+          <p>Carregando perfil...</p>
         </div>
       );
     }
-   
 
-    if (error || !user) {
-      return (
+    if(error || !user) {
+      return(
         <div className="error-container">
-          <h2> ❌ {error}</h2>
-          <button onClick={ () => window.location.reload()}>Tentar novamente</button>
-
+          <h2>❌ {error}</h2>
+          <button onClick={ () => window.location.reload()}> Tentar novamente</button>
         </div>
       )
     }
@@ -57,8 +57,41 @@ export default function App() {
     return(
       <div className="app-container">
         <div className="profile-card"></div>
-        
+         <header className="profile-header">
+            <img src={user.avatar_url} alt={user.name} className="avatar"/>
+            <div className="user-info">
+            <h1>{user.name || user.login}</h1>
+            <p>{user.bio} || "sem bio disponivel"</p>
+            <div className="status">
+              <span><strong>{user.followers}</strong></span>
+              <span><strong>{user.followers}</strong></span>
+            </div>
+            <a href={user.html_url} target="_blank" rel="noreferrer"
+              className="gitghub-link">
+                Ver perfil completo
+            </a>
+            </div>
+         </header>
+
+         <main className="repos-section">
+          <h2>Repositórios  recentes</h2>
+          <div className="repos-grid">
+            {repos.map(repo => (
+              <div key={repo.id} className="repo-card">
+                <h3>{repo.name}</h3>
+                <p>{repo.descriptiom || "sem descriçao."}</p>
+                <div className="repo-footer">
+                  <span>{repo.stargazers_count} </span>
+                  <span>{repo.forks_count} </span>
+                  <a href="{repo.html_url}" target="_blank" rel="noreferrer">
+                  Acessar
+                  </a>
+                </div>
+              </div>
+              ))}
+          </div>
+         </main>
       </div>
     );
+}
 
-  }
